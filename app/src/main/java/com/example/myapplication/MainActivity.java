363c42cf2b92f3f0eef1fe1d8370485b2f6cc927
pokhengly <henglypok21012021@gmail.com>
2023-09-22 03:03:47 +0700

    test1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,4 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main); //
     }
     // vireak
+
+    public void test() {
+        System.out.println("This is from vireak");
+    }
 }
